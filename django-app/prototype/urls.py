c5207dad848c0bd892f6c1277abb01fdb342fd15
Kat Chuang <katychuang@gmail.com>
2013-04-08 15:27:27 -0400

    moved django files to django branch

--- django-app/prototype/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-from django.views.generic import DetailView, ListView
-from prototype.models import Users,Tweets
-
-urlpatterns = patterns('',
-	url(r'^$', 'wpe.prototype.views.home', name='random-series'),
-)
